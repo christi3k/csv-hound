9302706513c8e3bac316891fb2bbf84e00b4e761
Christie Koehler <ck@christi3k.net>
2019-06-24 22:54:08 -0700

    Initial module layout with logging.

---
+++ csvhound/__init__.py
@@ -0,0 +1,25 @@
+import logging
+
+# set up logging
+# TODO move to helper function/module?
+
+# create new logger with library's name
+logger = logging.getLogger(__name__)
+
+# set null handler
+logger.addHandler(logging.NullHandler())
+
+# set logging level
+logger.setLevel(logging.DEBUG)
+
+# create a new console stream handler, set its debug level, formatting and attach
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.DEBUG)
+logger.addHandler(consoleHandler)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', '%m/%d/%Y %I:%M:%S %p')
+consoleHandler.setFormatter(formatter)
+
+logger.debug('module loaded')
+
+def do_something():
+    logger.debug('do something')
---
+++ example.py
@@ -0,0 +1,8 @@
+import csvhound
+# import logging
+
+
+# how to override library logging level:
+# logging.getLogger('csvhound').setLevel(logging.WARNING)
+
+csvhound.do_something()
