256ff60004df226e214283fb2578c8a8086d24a5
Christie Koehler <ck@christi3k.net>
2019-06-28 18:59:28 -0700

    Start incorporating python-prompt-toolkit.

--- csvhound/cli.py
+++ csvhound/cli.py
@@ -1,55 +1,149 @@
 #!/usr/bin/env python
 """
-Simple example of a full screen application with a vertical split.
-
-This will show a window on the left for user input. When the user types, the
-reversed input is shown on the right. Pressing Ctrl-Q will quit the application.
+Start of csvhound full screen CLI powered by python-prompt-toolkit.
 """
 import io
 
 from prompt_toolkit.application import Application
+from prompt_toolkit.application.current import get_app
+from prompt_toolkit.completion import PathCompleter, WordCompleter
 from prompt_toolkit.buffer import Buffer
 from prompt_toolkit.key_binding import KeyBindings
+from prompt_toolkit.key_binding.bindings.focus import (
+    focus_next,
+    focus_previous,
+)
 from prompt_toolkit.layout.containers import (
+    Float,
+    FloatContainer,
     HSplit,
     VSplit,
     Window,
     WindowAlign,
 )
-from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl
+from prompt_toolkit.layout.controls import BufferControl, FormattedTextControl, DummyControl
 from prompt_toolkit.layout.layout import Layout
-
+from prompt_toolkit.layout.menus import CompletionsMenu
+from prompt_toolkit.layout.processors import (BeforeInput)
+from prompt_toolkit.widgets import Box, Button, Frame, Label, TextArea
 
 import csvhound.core
 
 model = csvhound.core.BaseHound()
-table = model.get_table_from_file('sample-data/pydata-event.csv')
+# table = model.get_table_from_file('sample-data/pydata-event.csv')
 # model.get_columns()
 # exit()
 # model.describe_table()
 # model.distinct_values('Size', with_count=False)
 
-# 3. Create the buffers
-#    ------------------
-
+# create buffers
 left_buffer = Buffer()
 right_buffer = Buffer()
 
-# left_buffer.text= "THIS IS WHERE TABLE INFO SHOULD GO"
-output = io.StringIO()
-model.describe_table(output=output)
-left_buffer.text = output.getvalue()
-output.close()
+# sample text to get us going
+left_buffer.text = 'test\none\ntwo\nthree'
 
-right_buffer.text = "MENU\n--------\nOpen File..\n"
-# 1. First we create the layout
-#    --------------------------
-
+# create windows; you need a Windor and a BufferControl to display Buffer
+# content
 left_window = Window(BufferControl(buffer=left_buffer))
-right_window = Window(BufferControl(buffer=right_buffer), width=15)
+right_window = Window(BufferControl(buffer=right_buffer))
 
+# define initial titlebar text
+def get_titlebar_text():
+    return [
+        ('class:title', ' CSV Hound'),
+        ('class:title', ' (Press [Ctrl-Q] to quit.)'),
+    ]
 
-body = VSplit([
+def get_status_text(message):
+    return [
+        ('class:red', '     '+message),
+    ]
+
+# accept_handler for open command
+def do_open(_):
+
+    model = csvhound.core.BaseHound()
+    # this is only for initial PoC!
+    # TODO: add validation, etc.
+    table = model.get_table_from_file(_.text)
+    # model.get_columns()
+    # exit()
+    output = io.StringIO()
+
+    model.describe_table(output=output)
+    contents = output.getvalue()
+    output.close()
+
+    # model.distinct_values('Size', with_count=False)
+
+    left_buffer.text = contents
+    # right_buffer.text = _.text
+    # hide previous command buffer control
+    command_area.children[0].content = DummyControl()
+    status_area.content = FormattedTextControl(get_status_text('file open: ' + _.text))
+    app = get_app()
+    app.layout.focus(left_buffer)
+
+def do_inspect(_):
+    right_buffer.text = "INSPECT:\n"+_.text
+    # hide previous command buffer control
+    command_area.children[0].content = DummyControl()
+    status_area.content = FormattedTextControl(get_status_text('inspect file: ' + _.text))
+    app = get_app()
+    app.layout.focus(left_buffer)
+
+# to build a TextArea without the widget, you need the following:
+#     :class:`~prompt_toolkit.buffer.Buffer`,
+#     :class:`~prompt_toolkit.layout.BufferControl` and
+#     :class:`~prompt_toolkit.layout.Window`.
+# let's try building this incrementally so we can sub in different accept_handlers
+
+# class prompt_toolkit.buffer.Buffer(completer=None, auto_suggest=None, history=None, validator=None, tempfile_suffix=u'', name=u'', complete_while_typing=False, validate_while_typing=False, enable_history_search=False, document=None, accept_handler=None, read_only=False, multiline=True, on_text_changed=None, on_text_insert=None, on_cursor_position_changed=None, on_completions_changed=None, on_suggestion_set=None)
+
+# create a command buffer for a given command, with named accept_handler
+def create_cmd_buffer(command):
+    completer=PathCompleter()
+    # if command == 'open' or command == 'inspect':
+        # completer=PathCompleter()
+    # else:
+        # completer=None
+    accept_handler = globals()['do_' + command]
+    return Buffer(
+        completer=completer,
+        name='cmd_buffer_'+command,
+        accept_handler=accept_handler,
+        multiline=False
+    )
+
+# create command buffer control with given command buffer
+def create_cmd_control(buffer, prompt='>>>> '):
+    return BufferControl(
+            buffer,
+            input_processors=[BeforeInput(prompt, style='class:text-area.prompt')]
+    )
+
+# cmd_buffer_open = create_cmd_buffer("open")
+
+# class prompt_toolkit.layout.BufferControl(buffer=None, input_processors=None, include_default_input_processors=True, lexer=None, preview_search=False, focusable=True, search_buffer_control=None, menu_position=None, focus_on_click=False, key_bindings=None)
+# cmd_open_control = BufferControl(cmd_buffer_open, input_processors=[BeforeInput('>>> ', style='class:text-area.prompt')])
+
+# cmd_open_control = create_cmd_control(cmd_buffer_open, prompt='[Open file:] ')
+
+# command_window = Window(BufferControl(Buffer()))
+command_window = Window(height=1, style='class:line')
+command_area = HSplit([
+    command_window,
+    Window(height=1, char='-', style='class:line')
+    ])
+
+# text_area = TextArea(completer=PathCompleter(),height=1, prompt='>>> ', multiline=False, accept_handler=do_open_file)
+# text_area_hsplit = HSplit([
+    # text_area,
+    # Window(height=1, char='-', style='class:line')
+    # ])
+
+main_body = VSplit([
     left_window,
 
     # A vertical line in the middle. We explicitly specify the width, to make
@@ -61,63 +155,69 @@ body = VSplit([
     right_window,
 ])
 
-# As a demonstration. Let's add a title bar to the top, displaying "Hello world".
-
-# somewhere, because usually the default key bindings include searching. (Press
-# Ctrl-R.) It would be really annoying if the search key bindings are handled,
-# but the user doesn't see any feedback. We will add the search toolbar to the
-# bottom by using an HSplit.
-
-
-def get_titlebar_text():
-    return [
-        ('class:title', ' CSV Hound'),
-        ('class:title', ' (Press [Ctrl-Q] to quit.)'),
-    ]
+status_area = Window(
+                height=1,
+                content=DummyControl(),
+                align=WindowAlign.LEFT,
+                style='class:reverse')
 
 
-root_container = HSplit([
+float_content = HSplit([
     # The titlebar.
     Window(height=1,
            content=FormattedTextControl(get_titlebar_text),
-           align=WindowAlign.LEFT, style='reverse'),
-
+           align=WindowAlign.LEFT,
+           style='class:reverse'),
+    status_area,
     # Horizontal separator.
+    Window(height=1, char='-', style='class:line'),
+    command_area,
+    # text_area,
     # Window(height=1, char='-', style='class:line'),
-
-    # The 'body', like defined above.
-    body,
+    main_body
 ])
 
+root_container = FloatContainer(
+    content = float_content,
+    floats=[
+        Float(xcursor=True,
+              ycursor=True,
+              content=CompletionsMenu(max_height=16, scroll_offset=1))
+    ]
+)
 
-# 2. Adding key bindings
-#   --------------------
 
-# As a demonstration, we will add just a ControlQ key binding to exit the
-# application.  Key bindings are registered in a
-# `prompt_toolkit.key_bindings.registry.Registry` instance. We use the
-# `load_default_key_bindings` utility function to create a registry that
-# already contains the default key bindings.
 
 kb = KeyBindings()
+kb.add('c-n')(focus_next)
+kb.add('c-p')(focus_previous)
 
-# Now add the Ctrl-Q binding. We have to pass `eager=True` here. The reason is
-# that there is another key *sequence* that starts with Ctrl-Q as well. Yes, a
-# key binding is linked to a sequence of keys, not necessarily one key. So,
-# what happens if there is a key binding for the letter 'a' and a key binding
-# for 'ab'. When 'a' has been pressed, nothing will happen yet. Because the
-# next key could be a 'b', but it could as well be anything else. If it's a 'c'
-# for instance, we'll handle the key binding for 'a' and then look for a key
-# binding for 'c'. So, when there's a common prefix in a key binding sequence,
-# prompt-toolkit will wait calling a handler, until we have enough information.
+@kb.add('c-o')
+def _(event):
+    """
+    attempt to insert a file open command prompt
+    """
+    cmd_buffer_open = create_cmd_buffer("open")
+    cmd_open_control = create_cmd_control(cmd_buffer_open, prompt='[Open file:] ')
 
-# Now, There is an Emacs key binding for the [Ctrl-Q Any] sequence by default.
-# Pressing Ctrl-Q followed by any other key will do a quoted insert. So to be
-# sure that we won't wait for that key binding to match, but instead execute
-# Ctrl-Q immediately, we can pass eager=True. (Don't make a habit of adding
-# `eager=True` to all key bindings, but do it when it conflicts with another
-# existing key binding, and you definitely want to override that behaviour.
+    command_area.children[0].content = cmd_open_control 
 
+    app = get_app()
+    app.layout.focus(command_area)
+
+@kb.add('c-b')
+def _(event):
+    """
+    attempt to "inspect" a file 
+    (this is really to make sure we can add multiple commands with keybindings; c-i isn't working for some reason)
+    """
+    cmd_buffer_inspect = create_cmd_buffer("inspect")
+    cmd_inspect_control = create_cmd_control(cmd_buffer_inspect, prompt='[Inspect file:] ')
+
+    command_area.children[0].content = cmd_inspect_control 
+
+    app = get_app()
+    app.layout.focus(command_area)
 
 @kb.add('c-c', eager=True)
 @kb.add('c-q', eager=True)
@@ -133,29 +233,8 @@ def _(event):
     """
     event.app.exit()
 
-
-# Now we add an event handler that captures change events to the buffer on the
-# left. If the text changes over there, we'll update the buffer on the right.
-
-
-def default_buffer_changed(_):
-    """
-    When the buffer on the left changes, update the buffer on
-    the right. We just reverse the text.
-    """
-    # right_buffer.text = left_buffer.text[::-1]
-
-
-# left_buffer.on_text_changed += default_buffer_changed
-
-
-# 3. Creating an `Application` instance
-#    ----------------------------------
-
-# This glues everything together.
-
 application = Application(
-    layout=Layout(root_container, focused_element=left_window),
+    layout=Layout(root_container),
     key_bindings=kb,
 
     # Let's add mouse support!
@@ -166,10 +245,6 @@ application = Application(
     full_screen=True)
 
 
-# 4. Run the application
-#    -------------------
-
-
 def run():
     # Run the interface. (This runs the event loop until Ctrl-Q is pressed.)
     application.run()
@@ -177,3 +252,4 @@ def run():
 
 if __name__ == '__main__':
     run()
+
